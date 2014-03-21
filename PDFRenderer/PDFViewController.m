4058d428ce039765e4e47c8940803ebd08e5a713
beepscore <support@beepscore.com>
2014-03-20 21:37:17 -0700

    Add methods to draw PDF.

--- PDFRenderer/PDFViewController.m
+++ PDFRenderer/PDFViewController.m
@@ -27,6 +27,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self drawText];
+    [self showPDFFile];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +37,84 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+-(void)drawText
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    NSString* fileName = @"Invoice.PDF";
+
+    NSArray *arrayPaths =
+    NSSearchPathForDirectoriesInDomains(
+                                        NSDocumentDirectory,
+                                        NSUserDomainMask,
+                                        YES);
+    NSString *path = [arrayPaths objectAtIndex:0];
+    NSString* pdfFileName = [path stringByAppendingPathComponent:fileName];
+
+    NSString* textToDraw = @"Hello World";
+    CFStringRef stringRef = (__bridge CFStringRef)textToDraw;
+
+    // Prepare the text using a Core Text Framesetter.
+    CFAttributedStringRef currentText = CFAttributedStringCreate(NULL, stringRef, NULL);
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(currentText);
+
+    CGRect frameRect = CGRectMake(0, 0, 300, 50);
+    CGMutablePathRef framePath = CGPathCreateMutable();
+    CGPathAddRect(framePath, NULL, frameRect);
+
+    // Get the frame that will do the rendering.
+    CFRange currentRange = CFRangeMake(0, 0);
+    CTFrameRef frameRef = CTFramesetterCreateFrame(framesetter, currentRange, framePath, NULL);
+    CGPathRelease(framePath);
+
+    // Create the PDF context using the default page size of 612 x 792.
+    UIGraphicsBeginPDFContextToFile(pdfFileName, CGRectZero, nil);
+
+    // Mark the beginning of a new page.
+    UIGraphicsBeginPDFPageWithInfo(CGRectMake(0, 0, 612, 792), nil);
+
+    // Get the graphics context.
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+
+    // Put the text matrix into a known state. This ensures
+    // that no old scaling factors are left in place.
+    CGContextSetTextMatrix(currentContext, CGAffineTransformIdentity);
+
+    // Core Text draws from the bottom-left corner up, so flip
+    // the current transform prior to drawing.
+    CGContextTranslateCTM(currentContext, 0, 100);
+    CGContextScaleCTM(currentContext, 1.0, -1.0);
+
+    // Draw the frame.
+    CTFrameDraw(frameRef, currentContext);
+
+    CFRelease(frameRef);
+    CFRelease(stringRef);
+    CFRelease(framesetter);
+
+    // Close the PDF context and write the contents out.
+    UIGraphicsEndPDFContext();
+    
 }
-*/
+
+-(void)showPDFFile
+{
+    NSString* fileName = @"Invoice.PDF";
+
+    NSArray *arrayPaths =
+    NSSearchPathForDirectoriesInDomains(
+                                        NSDocumentDirectory,
+                                        NSUserDomainMask,
+                                        YES);
+    NSString *path = [arrayPaths objectAtIndex:0];
+    NSString* pdfFileName = [path stringByAppendingPathComponent:fileName];
+
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+
+    NSURL *url = [NSURL fileURLWithPath:pdfFileName];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView setScalesPageToFit:YES];
+    [webView loadRequest:request];
+
+    [self.view addSubview:webView];
+}
 
 @end
