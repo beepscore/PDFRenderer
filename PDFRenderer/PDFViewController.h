6584cabd0edc4572083c2aa258fee53f0304be65
beepscore <support@beepscore.com>
2014-03-20 20:33:11 -0700

    Add class PDFViewController

---
+++ PDFRenderer/PDFViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDFViewController.h
+//  PDFRenderer
+//
+//  Created by Steve Baker on 3/20/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFViewController : UIViewController
+
+@end
