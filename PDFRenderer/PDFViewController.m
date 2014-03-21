97f39b5359a1fb145c46111a403e21e05ec3a1f6
beepscore <support@beepscore.com>
2014-03-20 22:52:32 -0700

    Extract method, change some method names to start with 'create' to indicate returned object should be CFRelease d.
    Reference
    http://stackoverflow.com/questions/14064336/arc-and-cfrelease

--- PDFRenderer/PDFViewController.m
+++ PDFRenderer/PDFViewController.m
@@ -14,8 +14,7 @@
 
 @implementation PDFViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -23,39 +22,25 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self drawText];
     [self showPDFFile];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
--(void)drawText
-{
+-(void)drawText {
     NSString *pdfFileName = [self pathFileName];
 
     NSString* textToDraw = @"Hello World";
-    CFStringRef stringRef = (__bridge CFStringRef)textToDraw;
+    CTFramesetterRef framesetter = [self createFramesetterFromString:textToDraw];
 
-    // Prepare the text using a Core Text Framesetter.
-    CFAttributedStringRef currentText = CFAttributedStringCreate(NULL, stringRef, NULL);
-    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(currentText);
-
-    CGRect frameRect = CGRectMake(0, 0, 300, 50);
-    CGMutablePathRef framePath = CGPathCreateMutable();
-    CGPathAddRect(framePath, NULL, frameRect);
-
-    // Get the frame that will do the rendering.
-    CFRange currentRange = CFRangeMake(0, 0);
-    CTFrameRef frameRef = CTFramesetterCreateFrame(framesetter, currentRange, framePath, NULL);
-    CGPathRelease(framePath);
+    CTFrameRef frameRef = [self createFrameRef:framesetter];
 
     // Create the PDF context using the default page size of 612 x 792.
     UIGraphicsBeginPDFContextToFile(pdfFileName, CGRectZero, nil);
@@ -63,7 +48,6 @@
     // Mark the beginning of a new page.
     UIGraphicsBeginPDFPageWithInfo(CGRectMake(0, 0, 612, 792), nil);
 
-    // Get the graphics context.
     CGContextRef currentContext = UIGraphicsGetCurrentContext();
 
     // Put the text matrix into a known state. This ensures
@@ -79,16 +63,13 @@
     CTFrameDraw(frameRef, currentContext);
 
     CFRelease(frameRef);
-    CFRelease(stringRef);
     CFRelease(framesetter);
 
     // Close the PDF context and write the contents out.
     UIGraphicsEndPDFContext();
-    
 }
 
-- (NSString *)pathFileName
-{
+- (NSString *)pathFileName {
     NSString* fileName = @"Invoice.PDF";
     
     NSArray *arrayPaths =
@@ -101,19 +82,43 @@
     return pdfFileName;
 }
 
--(void)showPDFFile
-{
+// CoreFoundation naming convention for functions
+// Function name with copy or create means caller is responsible for releasing returned object
+// http://stackoverflow.com/questions/14064336/arc-and-cfrelease
+
+- (CTFramesetterRef)createFramesetterFromString:(NSString *)string {
+    CFStringRef stringRef = (__bridge CFStringRef)string;
+    
+    // Prepare the text using a Core Text Framesetter.
+    CFAttributedStringRef currentText = CFAttributedStringCreate(NULL, stringRef, NULL);
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(currentText);
+    // I think at this point it's safe to release currentText to avoid leak
+    CFRelease(currentText);
+    return framesetter;
+}
+
+- (CTFrameRef)createFrameRef:(CTFramesetterRef)framesetter {
+    CGRect frameRect = CGRectMake(0, 0, 300, 50);
+    CGMutablePathRef framePath = CGPathCreateMutable();
+    CGPathAddRect(framePath, NULL, frameRect);
+    
+    // Get the frame that will do the rendering.
+    CFRange currentRange = CFRangeMake(0, 0);
+    CTFrameRef frameRef = CTFramesetterCreateFrame(framesetter, currentRange, framePath, NULL);
+    CGPathRelease(framePath);
+    return frameRef;
+}
+
+- (void)showPDFFile {
     NSString* fileName = @"Invoice.PDF";
 
-    NSArray *arrayPaths =
-    NSSearchPathForDirectoriesInDomains(
-                                        NSDocumentDirectory,
-                                        NSUserDomainMask,
-                                        YES);
+    NSArray *arrayPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                              NSUserDomainMask,
+                                                              YES);
     NSString *path = [arrayPaths objectAtIndex:0];
-    NSString* pdfFileName = [path stringByAppendingPathComponent:fileName];
+    NSString *pdfFileName = [path stringByAppendingPathComponent:fileName];
 
-    UIWebView* webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
 
     NSURL *url = [NSURL fileURLWithPath:pdfFileName];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
