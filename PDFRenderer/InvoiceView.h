f5d092f37391b6c4ea5e5d83c843bb548eaef699
beepscore <support@beepscore.com>
2014-03-21 00:42:04 -0700

    Add InvoiceView

---
+++ PDFRenderer/InvoiceView.h
@@ -0,0 +1,13 @@
+//
+//  InvoiceView.h
+//  PDFRenderer
+//
+//  Created by Steve Baker on 3/21/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InvoiceView : UIView
+
+@end
---
+++ PDFRenderer/InvoiceView.m
@@ -0,0 +1,31 @@
+//
+//  InvoiceView.m
+//  PDFRenderer
+//
+//  Created by Steve Baker on 3/21/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import "InvoiceView.h"
+
+@implementation InvoiceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
